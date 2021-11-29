2458623ea53bcca62b8ebfba0b0513d220985de5
Kunal Shah <me@kunal.sh>
2021-11-29 11:15:29 -0500

    run anchor test

--- myanchorproject/programs/myepicproject/src/lib.rs
+++ myanchorproject/programs/myepicproject/src/lib.rs
@@ -1,14 +1,17 @@
+
 use anchor_lang::prelude::*;
+// use anchor_lang::Context;
+// use anchor_lang::prelude::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod myepicproject {
-    use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
-        Ok(())
-    }
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> ProgramResult {
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
